e05117f9191321990394c67cefc56ea300f08dfa
Petru Groza <pgroza@endava.com>
2017-05-12 17:31:25 +0300

    added HTTP Error Controller

--- src/main/java/com/endava/spring/controller/ErrorController.java
+++ src/main/java/com/endava/spring/controller/ErrorController.java
@@ -1,7 +1,32 @@
 package com.endava.spring.controller;
 
-/**
- * Created by pgroza on 5/12/2017.
- */
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class ErrorController {
+
+    @RequestMapping(value = {"/link403"}, method = RequestMethod.GET)
+    public String httpError403(ModelMap modelMap){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            modelMap.addAttribute("username", authentication.getName());
+        }
+        return "HTTP403";
+    }
+
+    @RequestMapping(value = {"/link404"}, method = RequestMethod.GET)
+    public String httpError404(ModelMap modelMap){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            modelMap.addAttribute("username", authentication.getName());
+        }
+        return "HTTP404";
+    }
 }
+
